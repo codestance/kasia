923410517557446bdad37f14b3968a69af2fb306
marrow00 <skrzynska.konstancja@gmail.com>
2017-03-19 20:25:26 +0100

    Add files via upload
    hobby subpage

---
+++ js/javascript-hobby.js
@@ -0,0 +1,18 @@
+/* H O B B Y*/
+var index=1;
+showDivs(index);
+
+function plusDivs(n) {
+  showDivs(index += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("hobbyPhoto");
+  if (n > x.length) {index = 1;}    
+  if (n < 1) {index = x.length;}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[index-1].style.display = "block";  
+}
